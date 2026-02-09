8aeca73724951c64cafff878ee38d097b5623099
electrovenik <englishmanaman@gmail.com>
2026-02-09 20:22:05 +0600

    asd

--- okurmen/settings.py
+++ okurmen/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-dev-okurmen')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['okurmentest.duckdns.org', 'localhost', '127.0.0.1']
 
 
 # Application definition
